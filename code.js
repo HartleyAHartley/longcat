d7daa919a5231242bac277748c4245d2bbcab9ca
Quest <wrdqex@gmail.com>
2017-05-18 12:35:33 -0400

    Longer scroll extension

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 function scroll(){
   var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  if(document.body.scrollTop+h+100 > document.body.scrollHeight){
-    document.body.style.height = document.body.scrollHeight + "px";
+  if(document.body.scrollTop+h+1000 > document.body.scrollHeight){
+    document.body.style.height = document.body.scrollHeight + 500 + "px";
   }
 }
